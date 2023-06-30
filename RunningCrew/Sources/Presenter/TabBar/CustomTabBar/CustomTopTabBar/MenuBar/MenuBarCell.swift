6713a5d1d19e985e2ceb75be8bba29bb0de354a5
lyoodong <dwryu3079@naver.com>
2023-06-30 20:18:37 +0900

    push

--- RunningCrew/Sources/Presenter/TabBar/CustomTabBar/CustomTopTabBar/MenuBar/MenuBarCell.swift
+++ RunningCrew/Sources/Presenter/TabBar/CustomTabBar/CustomTopTabBar/MenuBar/MenuBarCell.swift
@@ -29,6 +29,7 @@ class MenuBarCell: UICollectionViewCell {
         setupView()
     }
     
+    
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
