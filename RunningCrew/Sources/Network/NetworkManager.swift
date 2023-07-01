90c740e81c67fa2ec800d42884b94b7a6d57ad29
Kim-Coding <ptibabmgo@gmail.com>
2023-07-01 11:58:45 +0900

    fix: 사용하지 않은 pod 제거

--- RunningCrew/Sources/Network/NetworkManager.swift
+++ RunningCrew/Sources/Network/NetworkManager.swift
@@ -1,29 +1,29 @@
+////
+////  NetworkManager.swift
+////  RunningCrew
+////
+////  Created by Kim TaeSoo on 2023/06/04.
+////
 //
-//  NetworkManager.swift
-//  RunningCrew
+//import Foundation
+//import Alamofire
 //
-//  Created by Kim TaeSoo on 2023/06/04.
-//
-
-import Foundation
-import Alamofire
-
-class NetworkManager {
-    static let shared = NetworkManager()
-    static let mainurl = "https://runningcrew-test.ddns.net"
-    private init() {}
-    
-    func networkConnect(url: String, method: HTTPMethod, model: Codable) {
-        guard let url = URL(string: NetworkManager.mainurl + url) else { return }
-        
-        AF.request(url, method: method).responseDecodable(of: model.self) { response in
-            switch response.result {
-            case .success(let data):
-                print("네트워크 통신", data)
-            case .failure(let error):
-                print("statusCode", response.response?.statusCode)
-                print("네트워크 에러", error.localizedDescription)
-            }
-        }
-    }
-}
+//class NetworkManager {
+//    static let shared = NetworkManager()
+//    static let mainurl = "https://runningcrew-test.ddns.net"
+//    private init() {}
+//    
+//    func networkConnect(url: String, method: HTTPMethod, model: Codable) {
+//        guard let url = URL(string: NetworkManager.mainurl + url) else { return }
+//        
+//        AF.request(url, method: method).responseDecodable(of: model.self) { response in
+//            switch response.result {
+//            case .success(let data):
+//                print("네트워크 통신", data)
+//            case .failure(let error):
+//                print("statusCode", response.response?.statusCode)
+//                print("네트워크 에러", error.localizedDescription)
+//            }
+//        }
+//    }
+//}
--- RunningCrew/Sources/Network/RunningNoticeManager.swift
+++ RunningCrew/Sources/Network/RunningNoticeManager.swift
@@ -1,57 +1,57 @@
+////
+////  RunningNoticeManager.swift
+////  RunningCrew
+////
+////  Created by Kim TaeSoo on 2023/06/04.
+////
 //
-//  RunningNoticeManager.swift
-//  RunningCrew
+//import Foundation
 //
-//  Created by Kim TaeSoo on 2023/06/04.
 //
-
-import Foundation
-
-
-class RunningNoticeManager {
-    let mainURL = NetworkManager.mainurl
-    let id: String
-    var uri = "/api/running-notices/"
-    init (id: String) {
-        self.uri += id
-    }
-    func uploadFormData() {
-        let urlString =
-        
-        guard let url = URL(string: urlString) else {
-            print("Invalid URL")
-            return
-        }
-        
-        let parameters: [String: Any] = [
-            "title": "런닝 공지 제목",
-            "detail": "런닝 공지 내용",
-            "runningDateTime": "2023-06-04T10:00:00" // 예시 날짜 및 시간
-        ]
-        
-        Alamofire.upload(multipartFormData: { multipartFormData in
-            for (key, value) in parameters {
-                if let data = "\(value)".data(using: .utf8) {
-                    multipartFormData.append(data, withName: key)
-                }
-            }
-        }, to: url, method: .put) { result in
-            switch result {
-            case .success(let upload, _, _):
-                upload.responseJSON { response in
-                    // 응답 처리
-                    if let statusCode = response.response?.statusCode {
-                        print("Status Code: \(statusCode)")
-                    }
-                    
-                    if let resultData = response.result.value {
-                        print("Response Data: \(resultData)")
-                    }
-                }
-            case .failure(let error):
-                // 오류 처리
-                print("Upload Error: \(error)")
-            }
-        }
-    }
-}
+//class RunningNoticeManager {
+//    let mainURL = NetworkManager.mainurl
+//    let id: String
+//    var uri = "/api/running-notices/"
+//    init (id: String) {
+//        self.uri += id
+//    }
+//    func uploadFormData() {
+//        let urlString =
+//        
+//        guard let url = URL(string: urlString) else {
+//            print("Invalid URL")
+//            return
+//        }
+//        
+//        let parameters: [String: Any] = [
+//            "title": "런닝 공지 제목",
+//            "detail": "런닝 공지 내용",
+//            "runningDateTime": "2023-06-04T10:00:00" // 예시 날짜 및 시간
+//        ]
+//        
+//        Alamofire.upload(multipartFormData: { multipartFormData in
+//            for (key, value) in parameters {
+//                if let data = "\(value)".data(using: .utf8) {
+//                    multipartFormData.append(data, withName: key)
+//                }
+//            }
+//        }, to: url, method: .put) { result in
+//            switch result {
+//            case .success(let upload, _, _):
+//                upload.responseJSON { response in
+//                    // 응답 처리
+//                    if let statusCode = response.response?.statusCode {
+//                        print("Status Code: \(statusCode)")
+//                    }
+//                    
+//                    if let resultData = response.result.value {
+//                        print("Response Data: \(resultData)")
+//                    }
+//                }
+//            case .failure(let error):
+//                // 오류 처리
+//                print("Upload Error: \(error)")
+//            }
+//        }
+//    }
+//}
